c9e9f21d6bc6b2b9efe1352768dc668e5a593077
Vimal mishra <vm45231@gmail.com>
2025-08-04 16:02:04 +0530

    LayOut.jsx fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import Layout from './components/common/LayOut.jsx';
+
 import { AuthProvider } from './context/AuthContext.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
