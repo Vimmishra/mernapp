46189402197e56c08ae078d01282c8ba46411527
Vimal mishra <vm45231@gmail.com>
2025-08-17 12:40:59 +0530

    fixed

--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -1,7 +1,6 @@
 import { useAuth } from "@/context/AuthContext";
 import { useState } from "react";
 import { Mail, Lock } from "lucide-react";
-import { Link } from "react-router-dom";
 
 export default function Login() {
   const [form, setForm] = useState({ email: "", password: "" });
@@ -60,7 +59,7 @@ export default function Login() {
         </form>
         <p className="mt-4 text-center text-sm text-gray-500">
           Don't have an account?{" "}
-          <Link to="/register" className="text-green-600 hover:underline">Sign up</Link>
+          <a href="/register" className="text-green-600 hover:underline">Sign up</a>
         </p>
       </div>
     </div>
